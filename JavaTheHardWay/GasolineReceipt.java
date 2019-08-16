dc8e55e35049fe4a0c2c66d8b680619554f758bc
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2019-08-16 00:28:58 -0500

    Exercise 2: Study Drill

--- JavaTheHardWay/GasolineReceipt.java
+++ JavaTheHardWay/GasolineReceipt.java
@@ -4,12 +4,12 @@ public class GasolineReceipt {
     System.out.println( "|                        |" );
     System.out.println( "|      CORNER STORE      |" );
     System.out.println( "|                        |" );
-    System.out.println( "| 2015-03-29  04:38PM    |" );
+    System.out.println( "| 2019-08-16  12:26AM    |" );
     System.out.println( "|                        |" );
     System.out.println( "| Gallons:       10.870  |" );
-    System.out.println( "| Price/gallon: $ 2.089  |" );
+    System.out.println( "| Price/gallon: $ 2.810  |" );
     System.out.println( "|                        |" );
-    System.out.println( "| Fuel total:  $ 22.71   |" );
+    System.out.println( "| Fuel total:  $ 30.54   |" );
     System.out.println( "|                        |" );
     System.out.println( "+------------------------+" );
   }
