cd059e88fe21cbe02a4a32994c0932cf2b0c5716
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2019-08-15 20:59:54 -0500

    finish exercise 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg{
+  public static void main( String[] args) {
+    System.out.println( "I, " + System.getProperty("user.name") + ", am determined to learn how to code.");
+    System.out.println( "Today's date is August 15th, 2019." );
+  }
+}
