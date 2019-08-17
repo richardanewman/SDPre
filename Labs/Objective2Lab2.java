09baebcbca947064a7ca5a7aefb7dc34c85ae272
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2019-08-17 00:44:28 -0500

    Complete Objective 2 Lab 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2{
+  public static void main(String[] args){
+    String name = "Richard Newman";
+    int age = 100;
+    String state = "Colorado";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
