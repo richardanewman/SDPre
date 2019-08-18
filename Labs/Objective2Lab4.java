0cf988290fdc290c8cc179bc2be10ea03cfb826f
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2019-08-17 22:54:18 -0500

    Complete Objective 2 Lab 4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,11 @@
+public class Objective2Lab4 {
+  public static void main(String[] args){
+    double num1, num2, num3, average;
+    num1 = 10;
+    num2 = 5;
+    num3 = 32;
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println(average);
+  }
+}
