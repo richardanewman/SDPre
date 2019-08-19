6b7d6d1fa7c93c9e1597a6a0615da3177abfbaad
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2019-08-19 13:55:40 -0500

    Add birthday question to fix calculation error.

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -3,14 +3,18 @@ import java.util.Calendar;
 
 public class Objective3Lab3 {
   public static void main(String[] args) {
-    int birthYear, age, currentYear;
+    int birthYear, age, currentYear, birthday, yes, no;
     currentYear = Calendar.getInstance().get(Calendar.YEAR);
 
+    Scanner birthdayInput = new Scanner(System.in);
     Scanner input = new Scanner(System.in);
     System.out.println("How old are you?");
     age = input.nextInt();
 
-    birthYear = currentYear - age;
+    System.out.println("Have you celebrated your birthday? Answer 0 for yes or 1 for no only.");
+    birthday = birthdayInput.nextInt();
+
+    birthYear = (currentYear - age) - birthday;
 
     System.out.println("You were born in " + birthYear);
     input.close();
