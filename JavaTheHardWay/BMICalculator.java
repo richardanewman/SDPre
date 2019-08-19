54e6f22d5c60b08dacb7ed9fde8ae9e04a95eb22
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2019-08-19 12:53:06 -0500

    Add changes to program from Study Drill prompts.

--- JavaTheHardWay/BMICalculator.java
+++ JavaTheHardWay/BMICalculator.java
@@ -3,13 +3,24 @@ import java.util.Scanner;
 public class BMICalculator {
   public static void main(String[] args){
     Scanner keyboard = new Scanner(System.in);
-    double m, kg, bmi;
+    double heightFeet, heightInches, lbs, m, kg, bmi;
 
-    System.out.print("Your height in m: ");
-    m = keyboard.nextDouble();
 
-    System.out.print("Your weight in kg: ");
-    kg = keyboard.nextDouble();
+    System.out.println("Please enter your height in feet only and then remaining inches only.");
+    System.out.println("For example: 6' 1\" would be \"Your height (feet only):\" 6\"");
+    System.out.println("Next you would enter \"Your height (inches only):\" 1");
+    System.out.print("Plese enter your height in feet only: ");
+    heightFeet = keyboard.nextDouble();
+
+    System.out.print("Please enter the remaining inches of your height: ");
+    heightInches = keyboard.nextDouble();
+
+    m = ((heightFeet*12) + heightInches) / 39.37;
+
+    System.out.print("Your weight in pounds: ");
+    lbs = keyboard.nextDouble();
+
+    kg = lbs / 2.205;
 
     bmi = kg / (m*m);
 
